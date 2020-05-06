2ced7e3cf9fc1b01299f8d60a6f52d91be43d64c
Dominic Canare <dom@dominiccanare.com>
2020-05-06 15:34:56 -0500

    add parent project to source control

---
+++ PKDev/Source/PKDev/PKDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PKDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PKDEV_API APKDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
