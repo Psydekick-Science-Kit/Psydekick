9da56a12d7d2fe34b1bd7e8b114deaa3bf9665c3
Dominic Canare <dom@dominiccanare.com>
2020-05-09 05:25:16 -0500

    Implements universal ToString for StimConfigs

--- PKDev/Plugins/Psydekick/Source/Psydekick/Private/StimulusConfiguration.cpp
+++ PKDev/Plugins/Psydekick/Source/Psydekick/Private/StimulusConfiguration.cpp
@@ -3,7 +3,50 @@
 
 #include "StimulusConfiguration.h"
 
+#include "Psydekick.h"
+
 FString UStimulusConfiguration::ToString_Implementation()
 {
-	return GetDetailedInfo();
+	FString PropValues = "";
+
+	for (TFieldIterator<UProperty> PropertyIterator(GetClass()); PropertyIterator; ++PropertyIterator)
+	{
+		UProperty* Property = *PropertyIterator;
+		PropValues += Property->GetName() + "=";
+
+		if (UBoolProperty* BoolProperty = Cast<UBoolProperty>(Property))
+		{
+			PropValues += BoolProperty->GetPropertyValue_InContainer(this) ? "true" : "false";
+		}
+		else if (UIntProperty* IntProperty = Cast<UIntProperty>(Property))
+		{
+			PropValues.AppendInt(IntProperty->GetPropertyValue_InContainer(this));
+		}
+		else if (UFloatProperty* FloatProperty = Cast<UFloatProperty>(Property))
+		{
+			FString::SanitizeFloat(FloatProperty->GetPropertyValue_InContainer(this));
+		}
+		else if (UStrProperty* StringProperty = Cast<UStrProperty>(Property))
+		{
+			PropValues += StringProperty->GetPropertyValue_InContainer(this);
+		}
+		else if (UObjectProperty* ObjectProperty = Cast<UObjectProperty>(Property))
+		{
+			PropValues += "<" + ObjectProperty->GetPropertyValue_InContainer(this)->GetName() + ">";
+		}
+		else {
+			PropValues += "?";
+		}
+
+		PropValues += ",";
+	}
+
+	if (PropValues.Len() > 0)
+	{
+		return GetName() + "(" + PropValues.Mid(0, PropValues.Len() - 1) + ")";
+	}
+	else {
+		return GetName() + "()";
+	}
+
 }
