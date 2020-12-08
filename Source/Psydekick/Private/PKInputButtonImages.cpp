2f0b322cc1f30b896d61803c57471b7a58db848b
Dominic Canare <dom@greenlightgo.org>
2020-12-08 15:49:32 -0600

    Adds input action bound buttons

---
+++ Source/Psydekick/Private/PKInputButtonImages.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PKInputButtonImages.h"
+
+#include "GameFramework/InputSettings.h"
+#include "GameFramework/PlayerInput.h"
+
+UTexture2D* UPKInputButtonImages::FromKey(FKey Key)
+{
+	if(Key == EKeys::Gamepad_FaceButton_Top)
+	{
+		return FaceTop;
+	}
+	if(Key == EKeys::Gamepad_FaceButton_Right)
+	{
+		return FaceRight;
+	}
+	if(Key == EKeys::Gamepad_FaceButton_Bottom)
+	{
+		return FaceBottom;
+	}
+	if(Key == EKeys::Gamepad_FaceButton_Left)
+	{
+		return FaceLeft;
+	}
+
+	return nullptr;
+}
+
+UTexture2D* UPKInputButtonImages::FromAction(FName Action)
+{
+	TArray<FInputActionKeyMapping> ActionMappings;
+
+	UInputSettings* InputSettings = UInputSettings::GetInputSettings();
+	InputSettings->GetActionMappingByName(Action, ActionMappings);
+
+	for(FInputActionKeyMapping ActionMapping : ActionMappings)
+	{
+		UTexture2D* Image = FromKey(ActionMapping.Key);
+		if(Image)
+		{
+			return Image;
+		}
+	}
+
+	return nullptr;
+}
